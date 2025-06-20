8794f090370b8f41020a147a30352b064221b84f
NIkhil <nikhil_21b349cs@gecwyd.ac.in>
2025-06-20 15:20:22 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const keyPair = {
+  cuisines: "top-cuisine",
+  cities: "top-city",
+  chains: "top-chain",
+};
+
+Object.entries(keyPair).forEach(([buttonId, divId]) => {
+  const button = document.getElementById(buttonId);
+  const div = document.getElementById(divId);
+  button.addEventListener("click", function () {
+    Object.entries(keyPair).forEach(([otherBtnId, otherDivId]) => {
+      document.getElementById(otherBtnId).classList.remove("selected");
+      document.getElementById(otherDivId).style.display = "none";
+    });
+
+    button.classList.add("selected");
+    div.style.display = "block";
+  });
+});
+
+const seeMore = document.getElementById("see-more");
+const arrowBtn = document.getElementById("arrow-button");
+const arrow = arrowBtn.querySelector("#svg-arrow");
+
+const moreDiv = document.getElementsByClassName("hiden");
+arrowBtn.addEventListener("click", function () {
+  Array.from(moreDiv).forEach(function (div) {
+    div.classList.toggle("show-all");
+  });
+  seeMore.textContent =
+    seeMore.textContent === "See more" ? "See less" : "See more";
+  arrow.classList.toggle("rotated");
+});
