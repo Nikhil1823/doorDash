6ac24564cb1ad02e5f12de3a3a817d4014440eb5
NIkhil <nikhil_21b349cs@gecwyd.ac.in>
2025-06-21 18:23:06 +0530

    aligned that aside section in desktop site

--- index.js
+++ index.js
@@ -32,13 +32,45 @@ arrowBtn.addEventListener("click", function () {
   arrow.classList.toggle("rotated");
 });
 
-window.onscroll = function () {
-  const tabHeader = document.getElementById("tab-header");
-  console.log("hi");
-  console.log(window.innerWidth);
+// window.onscroll = function () {
+//   const tabHeader = document.getElementById("tab-header");
+//   const buttonDiv = document.getElementsByClassName("button-div-2 ");
 
-  if (document.documentElement.scrollTop > 150 && window.innerWidth >= 768) {
-    tabHeader.style.display = "flex";
-    console.log("scrolled psat 200");
-  }
-};
+//   console.log("hi");
+//   console.log(window.innerWidth);
+//   const logo = document.getElementById("hide-logo");
+//   console.log(buttonDiv);
+
+//   if (document.documentElement.scrollTop > 150 && window.innerWidth >= 768) {
+//     tabHeader.style.display = "flex";
+//     tabHeader.style.position = "sticky";
+//     buttonDiv[0].style.display = "flex";
+//     tabHeader.style.top = 0;
+//     tabHeader.style.backgroundColor = "white";
+//     logo.style.display = "block";
+//     tabHeader.style.paddingRight = "8px";
+//     // tabHeader.classList.add("tab-header-adjustment");
+//     console.log("scrolled psat 200");
+//   } else if (
+//     window.innerWidth >= 768 &&
+//     document.documentElement.scrollTop < 150
+//   ) {
+//     logo.style.display = "none";
+//     // buttonDiv[0].style.display - "flex";
+//     tabHeader.style.position = "absolute";
+//     tabHeader.style.top = "12px";
+//     tabHeader.style.backgroundColor = "transparent";
+//     tabHeader.style.paddingRight = 0;
+//   } else {
+//     tabHeader.style.display = "none";
+//   }
+// };
+
+// window.onload = function () {
+//   if (window.innerWidth < 768) {
+//     const buttonDiv = document.getElementsByClassName("button-div-2 ");
+//     const tabHeader = document.getElementById("tab-header");
+//     tabHeader.style.display = "none";
+//     buttonDiv[0].style.display = "none";
+//   }
+// };
